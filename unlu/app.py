0af65915adc9114cb11d4f3397b1444a361053c8
Esteban Iarza <iarzaesteban94@gmail.com>
2020-09-23 00:08:21 -0300

    Arrancando mis primeros pasitos con python

--- unlu/app.py
+++ unlu/app.py
@@ -1,6 +1,17 @@
+
+
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return "Hola Corazon de melon!"
+
+@app.route('/willy')
+def willy():
+    return "Hola willy!!!"   
+
+    
+if __name__ == '__main__':
+  	app.run(debug=true)
