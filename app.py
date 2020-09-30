58322c3f972356a55e8883f7a861f284b63e86b7
viccmedina <medina.vicc@gmail.com>
2020-09-30 12:29:45 -0300

    mejoramos el home

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-	return render_template('login.html')
+	return render_template('home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
