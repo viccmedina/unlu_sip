ad6b8af6f130e7b30620aa56e6ad40a71b48e4de
viccmedina <medina.vicc@gmail.com>
2020-10-01 22:56:34 -0300

    cambios en la vista, ahora mando datos para usar con jinja

--- distribuidora/core/provincia/views.py
+++ distribuidora/core/provincia/views.py
@@ -34,3 +34,48 @@ def list():
 	"""
 	provincia = Provincia.query.all()
 	return render_template('list_provincia.html', provincia=provincia)
+
+# TODO:: esto debe estar dentro de usuario_persona. 
+# para probar lo pongo aca
+@provincia_blueprint.route('/home_cliente')
+def home_cliente():
+	panel = []
+	mock_data = {
+		'titulo': 'Gestionar Nuevo Pedido',
+		'subtitulo': 'Nuevo Pedido',
+		'descripcion': 'Esto es una descripcion',
+		'boton': 'Nuevo Pedido'
+	}
+
+	panel.append(mock_data)
+	mock_data = {
+		'titulo': 'Gestionar Nuevo Pedido 2',
+		'subtitulo': 'Nuevo Pedido 2',
+		'descripcion': 'Esto es una descripcion 2',
+		'boton': 'Nuevo Pedido 2'
+	}
+	panel.append(mock_data)
+
+	collapse = []
+	mock_data = {
+	'titulo': 'Mis Datos',
+	'contenido': 'Estos son datos de Prueba'
+	}
+	collapse.append(mock_data)
+	
+	mock_data = {
+	'titulo': 'Mis Datos',
+	'contenido': 'Estos son datos de Prueba'
+	}
+
+	mock_data = {
+	'titulo': 'Pedidos Guardados',
+	'contenido': None
+	}
+	collapse.append(mock_data)
+
+	datos = {
+		'accesos_rapidos': panel,
+		'collapse': collapse
+	}
+	return render_template('home_cliente.html', datos=datos)
