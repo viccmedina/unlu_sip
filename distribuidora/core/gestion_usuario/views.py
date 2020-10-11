5f157578c8d3a1a97ccaeec59298ed97abe42f9c
viccmedina <medina.vicc@gmail.com>
2020-10-11 15:13:45 -0300

    logramos proteger al usuario admin y su interfaz, cualquier otro tipo de usuario va direccionado a otra pagina. problemas a la hora de crear usuario desde la interface, admin no hashea pass

--- distribuidora/core/gestion_usuario/views.py
+++ distribuidora/core/gestion_usuario/views.py
@@ -11,29 +11,35 @@ gestion_usuario = Blueprint('gestion_usuario', __name__, template_folder='templa
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        # Grab the user from our User Models table
+
         user = Usuario.query.filter_by(username=form.username.data).first()
-
-        # Check that the user was supplied and the password is right
-        # The verify_password method comes from the User object
-        # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
-        print(user.has_role('Gerencia'), flush=True)
+        print('el usuario tiene el rol: {}'.format(user.usuario_rol), flush=True)
         if user.check_password(form.password.data) and user is not None:
+            print('pass y username OK', flush=True)
             #Log in the user
+            if user.has_role('Gerencia'):
+                print('ES GERENCIA', flush=True)
+                login_user(user)
+                flash('Bienvenido.')
 
-            login_user(user)
-            flash('Logged in successfully.')
+                # If a user was trying to visit a page that requires a login
+                # flask saves that URL as 'next'.
+                next = request.args.get('next')
 
-            # If a user was trying to visit a page that requires a login
-            # flask saves that URL as 'next'.
-            next = request.args.get('next')
-
-            # So let's now check if that next exists, otherwise we'll go to
-            # the welcome page.
-            if next == None or not next[0]=='/':
-                next = url_for('admin.index')
+                # So let's now check if that next exists, otherwise we'll go to
+                # the welcome page.
+            
+                if next == None or not next[0]=='/':
+                    next = url_for('admin.index')
+            else:
+                print('NO ES GERENCIA', flush=True)
+                next = url_for('gestion_usuario.otro')
 
             return redirect(next)
+        else:
+            print('ALGO NO ESTA BIEN', flush=True)
+            print(user, flush=True)
+            print(user.check_password(form.password.data), flush=True)
     return render_template('login.html', form=form)
 
 @gestion_usuario.route('/logout', methods=['GET', 'POST'])
@@ -41,3 +47,7 @@ def logout():
     logout_user()    
     flash("You have been logged out.")
     return redirect(url_for('gestion_usuario.login'))
+
+@gestion_usuario.route('/otro', methods=['POST', 'GET'])
+def otro():
+    return render_template('otro.html')
