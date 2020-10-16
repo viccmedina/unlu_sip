33cada1f77580fcfde191be8bdba938788be330d
viccmedina <medina.vicc@gmail.com>
2020-10-15 23:49:21 -0300

    agregamos gestion de lista de precios al home del operador

---
+++ distribuidora/core/gestion_lista_precio/views.py
@@ -0,0 +1,67 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from distribuidora import db
+
+lista_precio = Blueprint('lista_precio', __name__, template_folder='templates')
+
+@lista_precio.route('/lista_precio', methods=['GET'])
+@login_required
+def index():
+	if current_user.has_role('Operador'):
+		return render_template('home_lista_precio.html', \
+			datos=current_user.get_mis_datos(),\
+			is_authenticated=current_user.is_authenticated, \
+			rol='operador')
+
+@lista_precio.route('/consultar', methods=['GET'])
+@login_required		
+def consultar_lista_precio():
+    return render_template('form_consultar_lista_precio.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+
+@lista_precio.route('/agregar', methods=['GET'])
+@login_required	
+def agregar():
+	return render_template('form_agregar_lista_precio.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+
+@lista_precio.route('/modificar', methods=['GET'])
+@login_required 
+def modificar():
+    return render_template('form_modificar_lista_precio.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+
+@lista_precio.route('/eliminar', methods=['GET'])
+@login_required 
+def eliminar():
+    return render_template('form_eliminar_lista_precio.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+
+@lista_precio.route('/exportar', methods=['GET'])
+@login_required	
+def exportar():
+	return render_template('exportar_lista_precio.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+
+@lista_precio.route('/importar', methods=['GET'])
+@login_required	
+def importar():
+	return render_template('importar_lista_precio.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
