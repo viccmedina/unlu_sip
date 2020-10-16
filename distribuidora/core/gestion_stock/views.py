a01baf80fb79dd288644223472b93f78af454164
viccmedina <medina.vicc@gmail.com>
2020-10-15 22:31:06 -0300

    subimos home de gestion de stock para usuario operador, no hay nada de logica

---
+++ distribuidora/core/gestion_stock/views.py
@@ -0,0 +1,48 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from distribuidora import db
+
+stock = Blueprint('stock', __name__, template_folder='templates')
+
+@stock.route('/stock', methods=['GET'])
+@login_required
+def index():
+	if current_user.has_role('Operador'):
+		return render_template('home_stock.html', \
+			datos=current_user.get_mis_datos(),\
+			is_authenticated=current_user.is_authenticated, \
+			rol='operador')
+
+@stock.route('/consultar', methods=['GET'])
+@login_required		
+def consultar_stock():
+    return render_template('form_consultar_stock.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+
+@stock.route('/agregar', methods=['GET'])
+@login_required	
+def agregar():
+	return render_template('form_agregar_movimiento.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+
+@stock.route('/exportar', methods=['GET'])
+@login_required	
+def exportar():
+	return render_template('exportar_movimientos.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
+
+@stock.route('/importar', methods=['GET'])
+@login_required	
+def importar():
+	return render_template('importar_movimientos.html', \
+    datos=current_user.get_mis_datos(), \
+    is_authenticated=current_user.is_authenticated, \
+    rol='operador')
