e285364051ec9a8db7d5c8bcce3277535b20944a
viccmedina <medina.vicc@gmail.com>
2020-10-15 23:49:21 -0300

    agregamos gestion de lista de precios al home del operador

--- distribuidora/__init__.py
+++ distribuidora/__init__.py
@@ -23,9 +23,12 @@ from distribuidora.core.gestion_usuario.views import gestion_usuario
 from distribuidora.core.gestion_stock.views import stock
 from distribuidora.core.gestion_cta_corriente.views import cta_corriente
 from distribuidora.core.gestion_producto.views import producto
+from distribuidora.core.gestion_lista_precio.views import lista_precio
+
 
 app.register_blueprint(stock)
 app.register_blueprint(core_blueprint, url_prefix='/')
 app.register_blueprint(gestion_usuario)
 app.register_blueprint(cta_corriente)
 app.register_blueprint(producto)
+app.register_blueprint(lista_precio)
