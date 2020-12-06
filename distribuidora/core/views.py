f0ef9b8978edccb11d29fe8b8f937b44fb290537
viccmedina <medina.vicc@gmai.com>
2020-12-06 19:48:37 -0300

    Revert "solucionamos error al no estar logueado y msj sin leer"
    This reverts commit 417897cbdc1e1a021e1fb23f8c41e18a53d1e30a.

--- distribuidora/core/views.py
+++ distribuidora/core/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, Blueprint, url_for, redirect
 from flask_login import  current_user
 from distribuidora import db
-from distribuidora.core.mensaje.helper import get_cantidad_msj_sin_leer
 from distribuidora.models.producto import Producto, Marca, UnidadMedida, ProductoEnvase
 
 # Definimos el Blueprint para manejas las vistas que son
@@ -17,30 +16,14 @@ def obtener_rol():
 	return rol
 
 
-def get_datos_extras():
-	result = dict()
-	if current_user.is_authenticated:
-		result['auth'] = current_user.get_mis_datos()
-		result['sin_leer'] =  get_cantidad_msj_sin_leer(current_user.get_id())
-	else:
-		result = None
-	return result
-
 @core_blueprint.route('/')
 def index():
 	"""
 	Nos mostrará el Home del sitio.
 	"""
 	rol = obtener_rol()
-	extras = get_datos_extras()
-	if extras is not None:
-		sin_leer = extras['sin_leer']
-		datos = extras['auth']
-	else:
-		sin_leer, datos = None, None
 	print(current_user.is_authenticated, flush=True)
 	return render_template('index.html', \
-		sin_leer = sin_leer,\
 		is_authenticated=current_user.is_authenticated, \
 		rol=rol, \
 		site='Home')
@@ -58,17 +41,9 @@ def contacto():
 	Nos devolverá la sección Contactos.
 	"""
 	rol = obtener_rol()
-	extras = get_datos_extras()
-	if extras is not None:
-		sin_leer = extras['sin_leer']
-		datos = extras['auth']
-	else:
-		sin_leer, datos = None, None
 	return render_template('contacto.html', \
 		is_authenticated=current_user.is_authenticated, \
 		rol=rol, \
-		datos=datos,\
-		sin_leer=sin_leer,\
 		site='Contactos')
 
 @core_blueprint.route('/nosotros')
@@ -76,19 +51,11 @@ def nosotros():
 	"""
 	Nos devolverá la sección Nosotros.
 	"""
-	
 	rol = obtener_rol()
-	extras = get_datos_extras()
-	if extras is not None:
-		sin_leer = extras['sin_leer']
-		datos = extras['auth']
-	else:
-		sin_leer, datos = None, None
 	return render_template('nosotros.html', \
 		is_authenticated=current_user.is_authenticated, \
 		rol=rol, \
-		sin_leer = sin_leer,\
-		datos=datos,\
+		datos=current_user.get_mis_datos(),\
 		site='Nosotros')
 
 @core_blueprint.route('/como_comprar')
@@ -97,15 +64,8 @@ def como_comprar():
 	Nos devolverá la sección Cómo Comprar.
 	"""
 	rol = obtener_rol()
-	extras = get_datos_extras()
-	if extras is not None:
-		sin_leer = extras['sin_leer']
-		datos = extras['auth']
-	else:
-		sin_leer, datos = None, None
 	return render_template('como_comprar.html', \
 		is_authenticated=current_user.is_authenticated, \
 		rol=rol, \
-		datos=datos,\
-		sin_leer=sin_leer,\
+		datos=current_user.get_mis_datos(),\
 		site='¿ Cómo Comparar ? ')
