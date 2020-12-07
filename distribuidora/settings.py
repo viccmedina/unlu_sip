bcc3d20fcd6bd6599c47f78bf751366f4c069ce1
viccmedina <medina.vicc@gmail.com>
2020-12-07 16:08:12 -0300

    probamos tema de variables de configuracion

--- distribuidora/settings.py
+++ distribuidora/settings.py
@@ -1,8 +1,9 @@
-DB_PATH = ''
-DB_SECRET_KEY = ''
-DATOS_PATH = ''
-
+DB_PATH = '/opt/unlu_sip/data.sqlite'
+DB_SECRET_KEY = 'mysecretkey'
+DATOS_PATH = '/opt/unlu_sip/distribuidora/datos/'
+"""
 try:
    from distribuidora.settings_local import *
 except ImportError:
     raise Exception("A settings_local.py file is required to run this project")
+"""
